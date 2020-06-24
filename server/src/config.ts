aaae66c0019e0a9f9cefae89d34ac53728f47d84
Jayadeep KM <kmjayadeep@gmail.com>
2020-06-24 17:19:07 +0530

    add mysql env

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,3 +3,10 @@ require('dotenv').config();
 
 export const PORT: number = parseInt(process.env.PORT || '4000');
 export const ENV: string = process.env.NODE_ENV || 'development';
+
+export const MYSQL = {
+  host: process.env.MYSQL_HOST || 'localhost',
+  database: process.env.MYSQL_DATABASE || 'dailytasks',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '',
+};
